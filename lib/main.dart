de8a7897e1c5bcd28d4253d6f14f04a7cc523fb2
董玉毛 <DYM@dongyumaodeMacBook-Pro.local>
2019-08-17 17:33:30 +0800

    导航栏两侧按钮

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/model/post.dart';
+import './demo/listview_demo.dart';
 
 void main() => runApp(App());
 
@@ -16,57 +17,40 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Widget _listitemBuilder(context, index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(
-            height: 16.0,
-          ),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subtitle,
-          ),
-          SizedBox(
-            height: 16.0,
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo'),
+        centerTitle: true,
+        title: Text(
+          'Demo',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+          ),
         elevation: 0.0, //导航栏下的阴影长度
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'navgation',
+          onPressed: (){},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'search',
+            onPressed: (){debugPrint('click search');},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            tooltip: 'horize',
+            onPressed: (){},
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listitemBuilder,
-      ),
+      body: ListViewDemo(),
     );
   }
 }
 
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'hello',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.black38),
-      ),
-    );
-  }
-}
+
