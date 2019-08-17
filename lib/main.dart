708ea12129e388c99853d7a2355bc5a9ea45513e
董玉毛 <DYM@dongyumaodeMacBook-Pro.local>
2019-08-17 17:45:13 +0800

    添加tabbarView

--- lib/main.dart
+++ lib/main.dart
@@ -19,38 +19,71 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Demo',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true, // 文字是否居中
+          title: Text('Demo',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              )),
+          elevation: 0.0, //导航栏下的阴影长度
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'navgation',
+            onPressed: () {},
           ),
-          textAlign: TextAlign.center,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () {
+                debugPrint('click search');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.more_horiz),
+              tooltip: 'horize',
+              onPressed: () {},
+            ),
+          ],
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38, //标签未选中颜色
+            indicatorColor: Colors.black54, //标签底部横线选中的颜色
+            indicatorSize: TabBarIndicatorSize.label, //标签底部横线大小
+            indicatorWeight: 1.0, //标签底部横线高度 
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.local_florist),
+              ),
+              Tab(
+                icon: Icon(Icons.change_history),
+              ),
+              Tab(
+                icon: Icon(Icons.directions_bike),
+              ),
+            ],
           ),
-        elevation: 0.0, //导航栏下的阴影长度
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'navgation',
-          onPressed: (){},
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: (){debugPrint('click search');},
-          ),
-          IconButton(
-            icon: Icon(Icons.more_horiz),
-            tooltip: 'horize',
-            onPressed: (){},
-          ),
-        ],
+        body: TabBarView(
+          children: <Widget>[
+            Tab(
+              icon: Icon(Icons.local_florist),
+            ),
+            Icon(
+              Icons.change_history,
+              size: 128.0,
+              color: Colors.black,
+            ),
+            Icon(
+              Icons.directions_bike,
+              size: 128.0,
+              color: Colors.black,
+            ),
+          ],
+        ),
       ),
-      body: ListViewDemo(),
     );
   }
 }
-
-
