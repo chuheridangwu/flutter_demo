f1c4976a0a16a571888e72f20e0010baa1ed860d
dongyumao <153100950@qq.com>
2019-08-18 14:17:10 +0800

    SliverSafeArea: widget显示在安全区域内,SliverPadding: Widget的外边距

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -7,24 +7,37 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-                childAspectRatio: 0.6 // 宽高比例调整
-                ),
-            delegate: SliverChildBuilderDelegate( // 设置显示的widget，个数和widget
-              (BuildContext context, int index) {
-                return Container(
-                  child:
-                      Image.network(posts[index].imageUrl, fit: BoxFit.cover),
-                );
-              },
-              childCount: posts.length, 
+          SliverSafeArea( // widget显示在安全区域内
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+// SliverGrid 基本使用
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 0.6 // 宽高比例调整
+          ),
+      delegate: SliverChildBuilderDelegate(
+        // 设置显示的widget，个数和widget
+        (BuildContext context, int index) {
+          return Container(
+            child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+          );
+        },
+        childCount: posts.length,
       ),
     );
   }
