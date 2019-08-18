20434c2a37c359f5835e9082a83983af8cb647aa
dongyumao <153100950@qq.com>
2019-08-18 14:12:52 +0800

    SliverGrid的基本使用,必须设置两个delegate,一个设置数量,间距,一个delegate设置显示的widget和个数

---
+++ lib/demo/sliver_demo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/model/post.dart';
+
+class SliverDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 0.6 // 宽高比例调整
+                ),
+            delegate: SliverChildBuilderDelegate( // 设置显示的widget，个数和widget
+              (BuildContext context, int index) {
+                return Container(
+                  child:
+                      Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+                );
+              },
+              childCount: posts.length, 
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
