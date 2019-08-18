8cc55e221d1a030da4670046b647c225c5237345
dongyumao <153100950@qq.com>
2019-08-18 15:49:14 +0800

    TextEditingController: 文本监听Controller,在dispose方法中需要提前销毁

--- lib/demo/from_demo.dart
+++ lib/demo/from_demo.dart
@@ -4,7 +4,8 @@ class FromDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Theme( // 覆盖之前的主题
+      body: Theme(
+        // 覆盖之前的主题
         data: Theme.of(context).copyWith(
           primaryColor: Colors.black,
         ),
@@ -22,24 +23,42 @@ class FromDemo extends StatelessWidget {
   }
 }
 
-
-
 class TextFleldDemo extends StatefulWidget {
   @override
   _TextFleldDemoState createState() => _TextFleldDemoState();
 }
 
 class _TextFleldDemoState extends State<TextFleldDemo> {
+  final textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // textEditingController.text = '~hi'; // 设置初始化文字
+    textEditingController.addListener(() {
+      debugPrint('inut: ${textEditingController.text}');
+    });
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose(); //需要主动销毁掉，不销毁会消耗资源
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      onChanged: (value){
-        print('input: $value');
-      },
-      onSubmitted: (value){
+      controller: textEditingController,
+      // onChanged: (value){
+      //   print('input: $value');
+      // },
+      onSubmitted: (value) {
         print('submit: $value');
       },
-      decoration: InputDecoration( //TextField的装饰效果
+      decoration: InputDecoration(
+        //TextField的装饰效果
         icon: Icon(Icons.subject),
         labelText: 'Title', // 输入框顶部文字
         hintText: 'enter the post title', // 占位文字
@@ -51,14 +70,12 @@ class _TextFleldDemoState extends State<TextFleldDemo> {
   }
 }
 
-
 // 设置主题颜色
 class ThemeDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color:Theme.of(context).accentColor,
+      color: Theme.of(context).accentColor,
     );
   }
 }
-
