8af2f927bf7263bc8ad128222977fd2feec53a7f
dongyumao <153100950@qq.com>
2019-08-19 22:19:51 +0800

    FlatButtom的基础使用

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -13,28 +13,96 @@ class MaterialComponents extends StatelessWidget {
           ListItem(
               title: 'FloatingActionButtonDemo',
               page: FloatingActionButtonDemo()),
+          ListItem(title: 'Button', page: ButtonDemo()),
         ],
       ),
     );
   }
 }
 
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final Widget flatButtonDemo = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        FlatButton(
+          child: Text('Button'),
+          onPressed: () {},
+          splashColor: Colors.grey, // 按钮点击的渐末效果颜色
+          textColor: Theme.of(context).accentColor,
+        ),
+        FlatButton.icon(
+          icon: Icon(Icons.add),
+          label: Text('Button'),
+          onPressed: () {},
+          splashColor: Colors.grey, // 按钮点击的渐末效果颜色
+        ),
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ButtonDemo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            flatButtonDemo,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _widgetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('_widgetDemo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 悬浮按钮 FloatingActionButton 的基础使用
 class FloatingActionButtonDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Widget _floatingActionButton = FloatingActionButton( //图标漂浮按钮
-      onPressed: (){},
+    final Widget _floatingActionButton = FloatingActionButton(
+      //图标漂浮按钮
+      onPressed: () {},
       child: Icon(Icons.add),
       backgroundColor: Colors.black87,
-      shape: BeveledRectangleBorder( // 改变形状
+      shape: BeveledRectangleBorder(
+        // 改变形状
         borderRadius: BorderRadius.circular(30.0), //切角
       ),
     );
 
-    final _floatingActionButtonExtended = FloatingActionButton.extended( //漂浮动作按钮
-        onPressed: (){},
-        label: Text('add'),
-        icon: Icon(Icons.add),
+    final _floatingActionButtonExtended = FloatingActionButton.extended(
+      //漂浮动作按钮
+      onPressed: () {},
+      label: Text('add'),
+      icon: Icon(Icons.add),
     );
 
     return Scaffold(
@@ -42,12 +110,14 @@ class FloatingActionButtonDemo extends StatelessWidget {
         title: Text('FloatingActionButtonDemo'),
       ),
       floatingActionButton: _floatingActionButton,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, //漂浮按钮的位置
-      bottomNavigationBar: BottomAppBar( // 底部工具栏
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerDocked, //漂浮按钮的位置
+      bottomNavigationBar: BottomAppBar(
+        // 底部工具栏
         child: Container(
           height: 80,
         ),
-        shape:  CircularNotchedRectangle(),
+        shape: CircularNotchedRectangle(), // 切出一个悬浮按钮的图形
       ),
     );
   }
