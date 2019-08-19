267f9e73bca0700b2811b030db616064c17788bf
dongyumao <153100950@qq.com>
2019-08-19 23:07:42 +0800

    ButtonBar : 一组带边距的横排显示按钮

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -40,30 +40,31 @@ class ButtonDemo extends StatelessWidget {
         ),
       ],
     );
-    
+
     // RaisedButtom 有背景颜色、阴影、
     final Widget RaisedButtomDemo = Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Theme(
-          data: Theme.of(context).copyWith( // 覆盖主题某几个特定的值
-            buttonColor: Theme.of(context).accentColor,
-            buttonTheme: ButtonThemeData(  // 重新创建button主题
+          data: Theme.of(context).copyWith(
+              // 覆盖主题某几个特定的值
+              buttonColor: Theme.of(context).accentColor,
+              buttonTheme: ButtonThemeData(
+                // 重新创建button主题
                 textTheme: ButtonTextTheme.primary,
                 // shape: BeveledRectangleBorder( //改变主题形状
                 //   borderRadius: BorderRadius.circular(5.0),
                 // ),
                 shape: StadiumBorder(), // 椭圆形状
-            )
-          ),
+              )),
           child: RaisedButton(
-          child: Text('Buttom'),
-          onPressed: () {},
-          splashColor: Colors.grey,
-          textColor: Colors.white,
-          color: Theme.of(context).accentColor, // 背景颜色
-          // textTheme: ButtonTextTheme.primary, //主题
-        ),
+            child: Text('Buttom'),
+            onPressed: () {},
+            splashColor: Colors.grey,
+            textColor: Colors.white,
+            color: Theme.of(context).accentColor, // 背景颜色
+            // textTheme: ButtonTextTheme.primary, //主题
+          ),
         ),
         SizedBox(
           width: 10.0,
@@ -79,32 +80,34 @@ class ButtonDemo extends StatelessWidget {
       ],
     );
 
-     // OutlineButton 带边框的按钮 
+    // OutlineButton 带边框的按钮
     final Widget OutlineButtonDemo = Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Theme(
-          data: Theme.of(context).copyWith( // 覆盖主题某几个特定的值
-            buttonColor: Theme.of(context).accentColor,
-            buttonTheme: ButtonThemeData(  // 重新创建button主题
+          data: Theme.of(context).copyWith(
+              // 覆盖主题某几个特定的值
+              buttonColor: Theme.of(context).accentColor,
+              buttonTheme: ButtonThemeData(
+                // 重新创建button主题
                 textTheme: ButtonTextTheme.primary,
                 // shape: BeveledRectangleBorder( //改变主题形状
                 //   borderRadius: BorderRadius.circular(5.0),
                 // ),
                 shape: StadiumBorder(), // 椭圆形状
-            )
-          ),
+              )),
           child: OutlineButton(
-          child: Text('Buttom'),
-          onPressed: () {},
-          splashColor: Colors.grey,
-          textColor: Colors.black,
-          color: Theme.of(context).accentColor, // 背景颜色
-          // textTheme: ButtonTextTheme.primary, //主题
-          borderSide: BorderSide( //边框颜色
-            color: Colors.black,
+            child: Text('Buttom'),
+            onPressed: () {},
+            splashColor: Colors.grey,
+            textColor: Colors.black,
+            color: Theme.of(context).accentColor, // 背景颜色
+            // textTheme: ButtonTextTheme.primary, //主题
+            borderSide: BorderSide(
+              //边框颜色
+              color: Colors.black,
+            ),
           ),
-        ),
         ),
         SizedBox(
           width: 10.0,
@@ -126,10 +129,64 @@ class ButtonDemo extends StatelessWidget {
         Container(
           width: 130.0,
           child: OutlineButton(
-            onPressed: (){},
+            onPressed: () {},
             child: Text('Button'),
           ),
         )
+      ],
+    );
+
+    // Expanded 占满可用屏幕的 按钮
+    final Widget ExpandedWidthButton = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          flex: 2, // 占用空间的比例
+          child: OutlineButton(
+            onPressed: () {},
+            child: Text('Button'),
+          ),
+        ),
+        SizedBox(
+          width: 12.0,
+        ),
+        Expanded(
+          child: OutlineButton(
+            onPressed: () {},
+            child: Text('Button'),
+          ),
+        ),
+      ],
+    );
+
+    // 一组带边距的横排显示按钮
+    final Widget ButtonBarDemo = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Theme(
+          data: Theme.of(context).copyWith(
+            buttonTheme: ButtonThemeData(
+              padding: EdgeInsets.symmetric(horizontal: 22.0),
+            )
+          ),
+          child:ButtonBar( //ButtonBar的边距根据 主题边距设定
+            children: <Widget>[
+              OutlineButton(
+                onPressed: (){},
+                child: Text('button'),
+              ),
+               OutlineButton(
+                onPressed: (){},
+                child: Text('button'),
+              ),
+               OutlineButton(
+                onPressed: (){},
+                child: Text('button'),
+              ),
+            ],
+          ) ,
+        ),
+          
       ],
     );
 
@@ -146,6 +203,8 @@ class ButtonDemo extends StatelessWidget {
             RaisedButtomDemo,
             OutlineButtonDemo,
             FixedWidthButton,
+            ExpandedWidthButton,
+            ButtonBarDemo
           ],
         ),
       ),
