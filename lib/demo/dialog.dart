80b9dd4342b97ce9af2db0b06605976e30ede041
dongyumao <153100950@qq.com>
2019-08-26 17:24:24 +0800

    AlertDialog的基础使用

--- lib/demo/dialog.dart
+++ lib/demo/dialog.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-enum SimpleDoaLogOption{
-  A,B,C
-}
+enum SimpleDoaLogOption { A, B, C }
+enum AlertDiaLogOption { Cancel, OK }
 
 class DialogDemo extends StatefulWidget {
   @override
@@ -11,31 +10,31 @@ class DialogDemo extends StatefulWidget {
 }
 
 class _DialogDemoState extends State<DialogDemo> {
-String _simple = 'nothing';
+  String _simple = 'nothing';
   //SimpleDialog
   _openSimpleDialog() async {
-    final option = await  showDialog(
+    final option = await showDialog(
       context: context,
-      builder: (BuildContext context){
+      builder: (BuildContext context) {
         return SimpleDialog(
           title: Text('SimpleDialog'),
           children: <Widget>[
             SimpleDialogOption(
               child: Text('Option A'),
-              onPressed: (){
-                Navigator.pop(context,SimpleDoaLogOption.A);
+              onPressed: () {
+                Navigator.pop(context, SimpleDoaLogOption.A);
               },
             ),
             SimpleDialogOption(
               child: Text('Option b'),
-              onPressed: (){
-                Navigator.pop(context,SimpleDoaLogOption.B);
+              onPressed: () {
+                Navigator.pop(context, SimpleDoaLogOption.B);
               },
             ),
             SimpleDialogOption(
               child: Text('Option c'),
-              onPressed: (){
-                Navigator.pop(context,SimpleDoaLogOption.C);
+              onPressed: () {
+                Navigator.pop(context, SimpleDoaLogOption.C);
               },
             ),
           ],
@@ -43,24 +42,65 @@ String _simple = 'nothing';
       },
     );
 
-  switch (option) {
-    case SimpleDoaLogOption.A:
-      setState(() {
-        _simple = 'selected: A';
-      });
-      break;
+    switch (option) {
+      case SimpleDoaLogOption.A:
+        setState(() {
+          _simple = 'selected: A';
+        });
+        break;
       case SimpleDoaLogOption.B:
-       setState(() {
-        _simple = 'selected: B';
-      });
-      break;
+        setState(() {
+          _simple = 'selected: B';
+        });
+        break;
       case SimpleDoaLogOption.C:
-       setState(() {
-        _simple = 'selected: C';
-      });
-      break;
-    default:
+        setState(() {
+          _simple = 'selected: C';
+        });
+        break;
+      default:
+    }
   }
+
+  String _alert = 'nothing';
+// alertDialog
+  _openAlertDialog() async {
+    final action = await showDialog(
+        context: context,
+        barrierDismissible: false, //必须点击弹窗才能关闭遮盖
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('AlertDiaLog'),
+            content: Text('Are you sure about this?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.pop(context, AlertDiaLogOption.Cancel);
+                },
+              ),
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context, AlertDiaLogOption.OK);
+                },
+              )
+            ],
+          );
+        });
+    switch (action) {
+      case AlertDiaLogOption.OK:
+        setState(() {
+          _alert = 'OK';
+        });
+        break;
+      case AlertDiaLogOption.Cancel:
+        setState(() {
+          _alert = 'cancel';
+        });
+        break;
+      default:
+    }
   }
 
   @override
@@ -80,10 +120,24 @@ String _simple = 'nothing';
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('ShowSimpleDialog ->$_simple',)
+                  Text(
+                    'ShowSimpleDialog ->$_simple',
+                  )
                 ],
               ),
-            )
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            InkWell(
+              onTap: _openAlertDialog,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('ShowAlertDialog ->$_alert'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
