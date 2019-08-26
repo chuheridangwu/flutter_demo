fd5c03f3929b9ec7e788a814af49c2f8de26ec89
dongyumao <153100950@qq.com>
2019-08-26 10:29:35 +0800

    CheckboxListTile基础用法

--- lib/demo/check_box_demo.dart
+++ lib/demo/check_box_demo.dart
@@ -19,23 +19,49 @@ class _CheckBoxDemoState extends State<CheckBoxDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            CheckboxListTile(
+              title: Text('CheckBoxItemA'),
+              subtitle: Text('desc'),
+              onChanged: (value){
+                setState(() {
+                  _checkBoxItemA = value;
+                });
+              },
+              value: _checkBoxItemA,
+              selected: _checkBoxItemA, //是否有选中状态
+              secondary: Icon(Icons.add_alarm),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Checkbox(
-                  value: _checkBoxItemA,
-                  onChanged: (value){
-                    setState(() {
-                      _checkBoxItemA = value;
-                    });
-                  },
-                  activeColor: Colors.blue,
-                ),
-              ],
+              children: <Widget>[],
             ),
           ],
         ),
       ),
-    );;
+    );
+  }
+}
+
+class CheckBoxBasis extends StatefulWidget {
+  @override
+  _CheckBoxBasisState createState() => _CheckBoxBasisState();
+}
+
+class _CheckBoxBasisState extends State<CheckBoxBasis> {
+  bool _checkBoxItemA = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Checkbox(
+        value: _checkBoxItemA,
+        onChanged: (value) {
+          setState(() {
+            _checkBoxItemA = value;
+          });
+        },
+        activeColor: Colors.blue,
+      ),
+    );
   }
 }
