dc6777fbaa38da7b83b1047161fb209beb8a2950
dongyumao <153100950@qq.com>
2019-09-02 21:25:09 +0800

    添加chip / ChoiseChip / FilterChip /ActionChip 常用方法

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,6 +6,11 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tags = ['Apple', 'Banbana', 'Lemon'];
+  String _action = 'Nothing';
+  List<String> _selected = [];
+  String _choise = 'none';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,11 +48,110 @@ class _ChipDemoState extends State<ChipDemo> {
                         'https://resources.ninghao.net/images/overkill.png'),
                   ),
                 ),
-                Divider(color: Colors.grey,indent: 12.0,) //缩进
+                Chip(
+                  label: Text('City'),
+                  onDeleted: () {},
+                  deleteIcon: Icon(Icons.delete),
+                  deleteIconColor: Colors.redAccent,
+                  deleteButtonTooltipMessage: 'Remove this Item',
+                ),
+                Divider(
+                  color: Colors.grey,
+                  indent: 12.0,
+                ), //缩进
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: () {
+                        setState(() {
+                          _tags.remove(tag);
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 12.0,
+                ),
+                Container(
+                  child: Text('actionchip: $_action'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return ActionChip(
+                      label: Text(tag),
+                      onPressed: () {
+                        setState(() {
+                          _action = tag;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 12.0,
+                ),
+                Container(
+                  child: Text('FilterChip: ${_selected.toString()}'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: _selected.contains(tag),
+                      onSelected: (value){
+                        setState(() {
+                          if (_selected.contains(tag)) {
+                            _selected.remove(tag);
+                          }else{
+                            _selected.add(tag);
+                          }
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 12.0,
+                ),
+                Container(
+                  child: Text('ChoiseChip: $_choise'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return ChoiceChip(
+                      label: Text(tag),
+                      selected: _choise == tag,
+                      selectedColor: Colors.black12,
+                      onSelected: (value){
+                        setState(() {
+                          _choise = tag;
+                        });
+                      },
+                    );
+                  }).toList(),
+                )
               ],
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: () {
+          setState(() {
+            _tags = ['Apple', 'Banbana', 'Lemon'];
+            _selected = [];
+          });
+        },
       ),
     );
   }
