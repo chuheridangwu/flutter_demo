ba4d41de74091d505a23e00dcdccf070bea13fd9
chuheridangwu <yexumaomao@163.com>
2019-09-08 20:38:28 +0800

    使用rxdart第三方, Observable是基于Stream的封装,介绍Observable的基础用法

---
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:async';
+
+class RxDartdemo extends StatefulWidget {
+  @override
+  _RxDartdemoState createState() => _RxDartdemoState();
+}
+
+class _RxDartdemoState extends State<RxDartdemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RxDartdemo'),
+      ),
+      body: RxDartDemoHome(),
+    );
+  }
+}
+
+class RxDartDemoHome extends StatefulWidget {
+  @override
+  _RxDartDemoHomeState createState() => _RxDartDemoHomeState();
+}
+
+class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+      Observable<String> _observable = 
+      // Observable(Stream.fromIterable(['hello','你好']));
+      // Observable.fromFuture(Future.value('hello~'));
+      // Observable.fromIterable(['nihao','hello~']);
+      // Observable.just('hello~');
+      Observable.periodic(Duration(seconds: 3),(x) => x.toString()); //间隔三秒，调用一次值，X是重复的次数
+      _observable.listen(print);
+      
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
