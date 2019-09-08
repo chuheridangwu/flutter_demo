8c2d93e01c110aa3d71ef9b95e65801cefdc217c
chuheridangwu <yexumaomao@163.com>
2019-09-08 23:22:51 +0800

    使用StreamSink 对文本的值进行改变,封装传值类

--- lib/demo/bloc/counter_bloc_demo.dart
+++ lib/demo/bloc/counter_bloc_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class CounterHome extends StatelessWidget {
@@ -5,10 +7,17 @@ class CounterHome extends StatelessWidget {
   Widget build(BuildContext context) {
     CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return Center(
-      child: ActionChip(
-        label: Text('0'),
-        onPressed: (){
-          _counterBloc.log();
+      child: StreamBuilder(
+        initialData: 0,
+        stream: _counterBloc.count,
+        builder: (context, snapshot) {
+          return ActionChip(
+            label: Text('${snapshot.data}'),
+            onPressed: () {
+              _counterBloc.log();
+              _counterBloc.counter.add(1);
+            },
+          );
         },
       ),
     );
@@ -21,19 +30,20 @@ class CounterActionButton extends StatelessWidget {
     CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: (){
+      onPressed: () {
         _counterBloc.log();
+        _counterBloc.counter.add(1);
       },
     );
   }
 }
 
-class CounterProvider extends InheritedWidget{
+class CounterProvider extends InheritedWidget {
   final Widget child;
   final CounterBloc bloc;
-  CounterProvider({this.bloc,this.child}) : super(child: child);
+  CounterProvider({this.bloc, this.child}) : super(child: child);
 
-  static CounterProvider of(BuildContext context){
+  static CounterProvider of(BuildContext context) {
     return context.inheritFromWidgetOfExactType(CounterProvider);
   }
 
@@ -43,8 +53,30 @@ class CounterProvider extends InheritedWidget{
   }
 }
 
-class CounterBloc{
-  void log(){
+class CounterBloc {
+  int _count = 0;
+  final _counterActionController = StreamController<int>();
+  StreamSink<int> get counter => _counterActionController.sink;
+
+  final _counterController = StreamController<int>();
+  Stream<int> get count => _counterController.stream;
+
+  CounterBloc() {
+    _counterActionController.stream.listen(onData);
+  }
+
+  void onData(int data) {
+    print('$data');
+    _count = data + _count;
+    _counterController.add(_count);
+  }
+
+  void disponse() {
+    _counterActionController.close();
+    _counterController.close();
+  }
+
+  void log() {
     print('CounterBloc');
   }
 }
