a6dffcd681cd43e4622bc17c52f65410d9dd863d
chuheridangwu <yexumaomao@163.com>
2019-09-08 23:59:20 +0800

    使用http进行请求,对请求的结果json数据转模型

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HttpDemo extends StatefulWidget {
   @override
@@ -8,8 +12,60 @@ class HttpDemo extends StatefulWidget {
 class _HttpDemoState extends State<HttpDemo> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+      ),
+      body: HttpDemoHome(),
     );
   }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+    fetchPost().then((value) => print(value));
+  }
+
+  Future<List<Post>> fetchPost() async {
+    final response =
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+          .map<Post>((item) => Post.fromJson(item))
+          .toList();
+      return posts;
+    } else {
+      throw Exception('Failed to fecth posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String desc;
+  final String img;
+  final String author;
+
+  Post(this.desc, this.img, this.title, this.id, this.author);
+
+  Post.fromJson(Map json)
+      : title = json['title'],
+        desc = json['description'],
+        img = json['imgUrl'],
+        id = json['id'],
+        author = json['author'];
 }
