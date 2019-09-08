9a53fcab9755e66e6d3708b0220a1e0c538c33bc
chuheridangwu <yexumaomao@163.com>
2019-09-09 00:45:43 +0800

    AnimationController 的使用介绍

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -25,12 +25,17 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
   void initState() {
     super.initState();
     _animationControllerDemo = AnimationController(
-        duration: Duration(milliseconds: 1000), vsync: this //防止屏幕外的效果消耗资源
+        value: 32.0, //初始值
+        lowerBound: 0.0, // 最小的值
+        upperBound: 100.0, //最大的值
+        duration: Duration(milliseconds: 3000),
+        vsync: this //防止屏幕外的效果消耗资源
         );
     _animationControllerDemo.addListener(() {
       print('${_animationControllerDemo.value}');
+      setState(() {});
     }); // 监听动画变化
-    _animationControllerDemo.forward(); //开始播放动画
+    // _animationControllerDemo.forward(); //开始播放动画
   }
 
   @override
@@ -41,11 +46,23 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: ActionChip(
+        label: Text('${_animationControllerDemo.value}'),
+        onPressed: () {
+          _animationControllerDemo.forward();
+        },
+      ),
+    );
   }
 }
 
 /**
  * vsync: 是一个TickerProvider，通知当前类更改帧的值
+ * AnimationController 是一个动画监听控制器，可以设置动画时间，监听动画时间，在不需要动画的时候移除，dispose方法中
+ * AnimationController 有初始值，最小值，最大值，在动画中时改变状态，需要在监听器中做改动
+ * 
+ * 
+ * 
  * 
  */
