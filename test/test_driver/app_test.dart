a973feff9d36022a333423a61b887f1127632359
dongyumao <153100950@qq.com>
2019-09-16 14:22:23 +0800

    最后一课,集成测试程序中某功能,按钮的点击功能

--- test/test_driver/app_test.dart
+++ test/test_driver/app_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:flutter_driver/flutter_driver.dart' as prefix0;
 import 'package:flutter_test/flutter_test.dart';
 
 
 void main(){
   group('App', (){
     FlutterDriver driver;
+
+    final actionChip = prefix0.find.byValueKey('actionChip');
+    final actionChipLabelText = prefix0.find.byValueKey('actionChipLabelText');
+
 
     setUpAll(() async { // 开始连接
       driver = await FlutterDriver.connect();
@@ -16,6 +21,16 @@ void main(){
       }
     });
 
+    test('start at 0', () async {
+      expect(await driver.getText(actionChipLabelText), '0');
+    });
+
+    test('increments the counter', () async {
+      await driver.tap(actionChip);
+      expect(await driver.getText(actionChipLabelText), '1');
+    });
+
   });
+  
 
 }
